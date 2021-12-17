44cd2acc65792bf15a826ba9a62a9b08f6c6023d
Berlin <Berlin@Apples-MacBook-Pro.local>
2021-12-17 22:09:25 +0500

    setup theme.js and added example in Landing

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -1,7 +1,19 @@
 import * as React from 'react';
+import { createStyles, makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles(theme =>
+    createStyles({
+        root: {
+            backgroundColor: theme.palette.primary.main,
+            height: '100vh',
+            width: '100%',
+        },
+    }),
+);
 
 const Landing = () => {
-    return <div className="Landing">Landing</div>;
+    const classes = useStyles();
+    return <div className={classes.root}>Landing</div>;
 };
 
 export default Landing;
